706de719e5ea95f64d9e04ed2e84eaf7ee438b9e
igor neves silva <100931488+igorneveskm@users.noreply.github.com>
2024-06-25 11:56:55 -0300

    Add files via upload

---
+++ aula_api/api/data.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    [
+        'nome' => 'igor',
+        'email' => 'igorneves@gmail' 
+    ],
+    [
+        'nome' => 'Anderson',
+        'email' => 'Anderson@gmail' 
+    ],
+    [
+        'nome' => 'Ana isa',
+        'email' => 'anaisa@gmail' 
+    ],
+    [
+        'nome' => 'Danielle',
+        'email' => 'dani@gmail' 
+    ],
+    [
+        'nome' => 'Ariany',
+        'email' => 'ariany@gmail' 
+    ],
+    [
+        'nome' => 'Maria Rita',
+        'email' => 'ritão@gmail' 
+    ],
+    [
+        'nome' => 'Ana',
+        'email' => 'anaclara@gmail' 
+    ],
+    [
+        'nome' => 'Lucas',
+        'email' => 'lucas@gmail' 
+    ],
+    [
+        'nome' => 'Maria Eduarda',
+        'email' => 'maria.duda@gmail' 
+    ],
+    [
+        'nome' => 'Brian',
+        'email' => 'brianneneco@gmail' 
+    ],
+
+];
+
+?>
